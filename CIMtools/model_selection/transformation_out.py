d8147f70cc92a44865170bdfd198a533a56085c0
tagir <tagirshin@gmail.com>
2018-11-14 13:05:15 +0300

    Added documentation

--- CIMtools/model_selection/transformation_out.py
+++ CIMtools/model_selection/transformation_out.py
@@ -9,15 +9,68 @@ from sklearn.utils.validation import indexable
 
 
 class TransformationOut(BaseCrossValidator):
+    """Transformation-out cross-validator
+
+        Provides train/test indices to split data in train/test sets.
+        Split dataset into k consecutive folds (without shuffling by default).
+        Every fold includes all reactions of each transformation.
+        Each fold is then used once as a validation (test set) while the k - 1 remaining
+        folds form the training set. Test set includes only reactions with conditions
+        that appeared in other reactions.
+        Splits n times with different randomization in each repetition.
+
+
+        Parameters
+        ----------
+        n_splits : int, default=5
+            Number of folds. Must be at least 2.
+        n_repeats : int, default=5
+             Number of times cross-validator needs to be repeated.
+        shuffle : boolean, optional
+            Whether to shuffle the data before splitting into batches."""
+
     def __init__(self, n_splits=5, n_repeats=5, shuffle=False):
         self.n_splits = n_splits
         self.shuffle = shuffle
         self.n_repeats = n_repeats
 
     def get_n_splits(self, X=None, y=None, groups=None):
+        """Returns the number of splitting iterations in the cross-validator
+        Parameters
+        ----------
+        X : object
+            Always ignored, exists for compatibility.
+            ``np.zeros(n_samples)`` may be used as a placeholder.
+        y : object
+            Always ignored, exists for compatibility.
+            ``np.zeros(n_samples)`` may be used as a placeholder.
+        groups : array-like, with shape (n_samples,), optional
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+        Returns
+        -------
+        n_splits : int
+            Returns the number of splitting iterations in the cross-validator.
+        """
         return self.n_splits
 
     def split(self, X, y=None, groups=None):
+        """Generate indices to split data into training and test set.
+        Parameters
+        ----------
+        X : list of reactions
+        y : array-like, of length n_samples
+            The target variable for supervised learning problems.
+        groups : array-like, with shape (n_samples,), optional
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+        Yields
+        ------
+        train : ndarray
+            The training set indices for that split.
+        test : ndarray
+            The testing set indices for that split.
+        """
         X, y, groups = indexable(X, y, groups)
         cgr = CGRpreparer()
         cgrs = [cgr.condense(r) for r in X]
@@ -71,7 +124,9 @@ class TransformationOut(BaseCrossValidator):
 
             for i in range(self.n_splits):
                 train_index = []
-                for fold in train_folds[:i] + train_folds[i+1:]:
+                for fold in train_folds[:i]:
+                    train_index.extend(fold)
+                for fold in train_folds[i+1:]:
                     train_index.extend(fold)
                 test_index = test_folds[i]
                 yield array(train_index), array(test_index)
