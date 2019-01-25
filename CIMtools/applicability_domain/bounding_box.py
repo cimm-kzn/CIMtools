5d6d961baebd554f2daaecd35d9d25fd7f9d6127
asima <asima.astana@outlook.com>
2019-01-25 11:17:24 +0600

    deleted y in fit (делаю некоторые странные вещи для Рамиля, просто потому что не всегда понимаю как работать с этим гитом)

--- CIMtools/applicability_domain/bounding_box.py
+++ CIMtools/applicability_domain/bounding_box.py
@@ -34,7 +34,7 @@ class Box(BaseEstimator):
     def __init__(self):
         pass
 
-    def fit(self, X, y=None):
+    def fit(self, X):
         """Find min and max values of every feature.
         Parameters
         ----------
